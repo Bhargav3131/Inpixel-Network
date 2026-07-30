5475051866ab302636c626ddb920dec533e3767e
Bhargav3131 <bhargavkalbhor3131@gmail.com>
2026-07-30 16:39:42 +0530

    temp: update Social Media plan price to Rs 1 for testing

--- api/payments/create-order.js
+++ api/payments/create-order.js
@@ -18,7 +18,7 @@ module.exports = async function (req, res) {
     const { plan, service, amount, client_name, client_phone, client_email } = req.body || {};
 
     const expectedAmounts = {
-      'socialmedia': 99900,
+      'socialmedia': 100,
       'webdevelopment-starter': 299900,
       'webdevelopment-pro': 599900,
       'aivideos': 99900,
