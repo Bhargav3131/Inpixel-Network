7481b4ce69df214acfdb17d81f23e0b10043b0a6
Bhargav3131 <bhargavkalbhor3131@gmail.com>
2026-08-09 09:35:22 +0530

    feat: add /quotation page with demo video, lifetime pricing, and Razorpay checkout integration

--- api/payments/create-order.js
+++ api/payments/create-order.js
@@ -22,7 +22,8 @@ module.exports = async function (req, res) {
       'webdevelopment-starter': 299900,
       'webdevelopment-pro': 599900,
       'aivideos': 99900,
-      'metaads': 299900
+      'metaads': 299900,
+      'quotation': 299900
     };
 
     let validAmount = false;
